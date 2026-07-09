48c090f9b78b8c5a3276446a0858338f76db383b
kkangmen <pickofee@gmail.com>
2026-07-09 17:56:09 +0900

    chore: 불필요한 import 제거

--- src/main/java/umc/fitme/domain/user/repository/UserRepository.java
+++ src/main/java/umc/fitme/domain/user/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import umc.fitme.domain.user.entity.User;
 
-import java.lang.reflect.Member;
 import java.util.Optional;
 
 @Repository
