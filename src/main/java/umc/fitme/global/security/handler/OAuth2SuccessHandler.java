29500a96b10363cb49458757b9f754f871a13058
kkangmen <pickofee@gmail.com>
2026-07-09 17:48:04 +0900

    feat: 소셜로그인 성공/실패 시 프론트 주소로 리다이렉트

--- src/main/java/umc/fitme/global/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/umc/fitme/global/security/handler/OAuth2SuccessHandler.java
@@ -1,23 +1,21 @@
 package umc.fitme.global.security.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 import umc.fitme.domain.user.entity.User;
 import umc.fitme.domain.user.exception.UserException;
 import umc.fitme.domain.user.exception.code.UserErrorCode;
 import umc.fitme.domain.user.repository.UserRepository;
-import umc.fitme.global.apiPayload.ApiResponse;
-import umc.fitme.global.apiPayload.code.BaseSuccessCode;
 import umc.fitme.global.security.dto.LoginResDto;
 import umc.fitme.global.security.entity.CustomOAuth2User;
-import umc.fitme.global.security.exception.code.SocialLoginSuccessCode;
 import umc.fitme.global.security.util.JwtUtil;
 
 import java.io.IOException;
@@ -31,7 +29,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final UserRepository userRepository;
 
     /***
-     * 함수 기능: 로그인 성공 시, accessToken/refreshToken 발급 후 응답
+     * 함수 기능: 로그인 성공 시, accessToken/refreshToken 발급 후 리다이렉트
      * @param request the request which caused the successful authentication
      * @param response the response
      * @param authentication the <tt>Authentication</tt> object which was created during
@@ -55,12 +53,12 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         log.info("발급된 accessToken: {}", accessToken);
         log.info("발급된 refreshToken: {}", refreshToken);
 
-        // 소셜 로그인 성공 시, 반환되는 응답 DTO
-        createLoginResDto(response, userId, name, accessToken, refreshToken);
+        // 소셜 로그인 성공 시, 프론트 주소로 리다이렉션
+        redirect(request, response, userId, name, accessToken, refreshToken);
     }
 
     /***
-     * 함수 기능: 생성된 accessToken, refreshToken과 함께 유저 정보 및 온보딩 여부도 담아 응답한다.
+     * 함수 기능: 생성된 accessToken, refreshToken과 함께 유저 정보 및 온보딩 여부도 담아 전달.
      * @param response 응답
      * @param userId 유저ID
      * @param name 유저이름
@@ -68,34 +66,37 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
      * @param refreshToken
      * @throws IOException
      */
-    private void createLoginResDto(HttpServletResponse response, Long userId, String name, String accessToken, String refreshToken) throws IOException {
+    private void redirect(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Long userId, String name, String accessToken, String refreshToken) throws IOException {
 
         // 온보딩 여부 조사
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         Boolean isOnboarded = user.getIsOnboarded();
 
-        // LoginResDto.UserInfo 응답 DTO 빌드
-        LoginResDto.UserInfo userInfo = LoginResDto.UserInfo.builder()
-                .userId(userId)
-                .name(name)
-                .isOnboarded(isOnboarded)
+        // Refresh Token은 보안상 쿠키로 전달
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+                .maxAge(1209600)
+                .path("/")
+//                .secure(true) // 배포 시 주석 해제 (HTTPS 환경)
+//                .sameSite("None") // 배포 시 주석 해제 (CORS)
+                .httpOnly(true)
                 .build();
 
-        // ObjectMapper 생성
-        ObjectMapper objectMapper = new ObjectMapper();
-        BaseSuccessCode successCode = SocialLoginSuccessCode.SOCIAL_LOGIN_SUCCESS;
+        response.addHeader("Set-Cookie", cookie.toString() );
 
-        // 전체 응답 DTO 생성 (LoginResDto)
-        ApiResponse<LoginResDto> responseBody = ApiResponse.onSuccess(successCode, LoginResDto.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .userInfo(userInfo)
-                .build());
+        // 프론트 URL로 리다이렉트 주소 조립
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3030/oauth2/callback")
+                .queryParam("accessToken", accessToken)
+                .queryParam("userId", userId)
+                .queryParam("name", name)
+                .queryParam("isOnboarded", isOnboarded)
+                .build()
+                .encode()
+                .toUriString();
 
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(successCode.getStatus().value());
-        // 응답 반환
-        objectMapper.writeValue(response.getOutputStream(), responseBody);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
