b98ee459f1b4a1e8c5343749577536abb82cbf6e
j2nooh <wisestorm01@gmail.com>
2026-07-18 18:12:44 +0900

    feat: 마감일 이메일 알림 발송 로직 추가

--- src/main/java/umc/fitme/domain/notify/repository/DeadlineEmailNotificationLogRepository.java
+++ src/main/java/umc/fitme/domain/notify/repository/DeadlineEmailNotificationLogRepository.java
@@ -1,12 +1,14 @@
 package umc.fitme.domain.notify.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.fitme.domain.notify.entity.DeadlineEmailNotificationLog;
 import umc.fitme.domain.notify.enums.DeadlineReminderType;
 import umc.fitme.domain.post.entity.Post;
 import umc.fitme.domain.user.entity.User;
 
 import java.time.LocalDate;
 
-public interface DeadlineEmailNotificationLogRepository {
+public interface DeadlineEmailNotificationLogRepository extends JpaRepository<DeadlineEmailNotificationLog, Long> {
 
     boolean existsByUserAndPostAndReminderTypeAndApplyEndAt(
             User user,
