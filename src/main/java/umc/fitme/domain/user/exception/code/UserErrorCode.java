6db306e1e5d3354615f51babb574a0634f30c2fc
terry <rlfox1209@naver.com>
2026-07-20 04:04:31 +0900

    refactor: 미사용 알림 설정 에러 코드 제거

--- src/main/java/umc/fitme/domain/user/exception/code/UserErrorCode.java
+++ src/main/java/umc/fitme/domain/user/exception/code/UserErrorCode.java
@@ -16,10 +16,6 @@ public enum UserErrorCode implements BaseErrorCode {
 
     PROFILE_UPDATE_EMPTY(HttpStatus.BAD_REQUEST, "USER400_1", "수정할 항목이 없습니다."),
     INTEREST_NOT_FOUND(HttpStatus.NOT_FOUND, "INTEREST404_1", "존재하지 않는 관심 분야가 포함되어 있습니다."),
-
-    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "NOTIFICATION400_1", "올바르지 않은 이메일 형식입니다."),
-    INVALID_TOGGLE_VALUE(HttpStatus.BAD_REQUEST, "NOTIFICATION400_2", "잘못된 요청 형식입니다."),
-    UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "NOTIFICATION500_1", "알림 설정 수정에 실패했습니다."),
     ;
 
     private final HttpStatus status;
