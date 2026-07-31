73381b899dbea3631dc8aad99ac3229606c7aaeb
kkangmen <pickofee@gmail.com>
2026-07-31 23:06:37 +0900

    refactor: 에러코드 가독성 개선

--- src/main/java/umc/fitme/domain/auth/exception/code/AuthErrorCode.java
+++ src/main/java/umc/fitme/domain/auth/exception/code/AuthErrorCode.java
@@ -9,36 +9,16 @@ import umc.fitme.global.apiPayload.code.BaseErrorCode;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements BaseErrorCode {
 
-    CODE_NOT_VALIDATE(HttpStatus.BAD_REQUEST,
-            "AUTH400_1",
-            "해당 인증번호는 만료되었습니다. 발급된 시간 기준 5분 이내로 인증번호를 입력해주세요."),
-    CODE_NOT_MATCH(HttpStatus.BAD_REQUEST,
-            "AUTH400_2",
-            "올바르지 않는 인증번호 입니다. 정확한 인증번호 6자리를 입력해주세요."),
-    CODE_ALREADY_USED(HttpStatus.BAD_REQUEST,
-            "AUTH400_3",
-            "이미 인증코드가 사용되었습니다."),
-    NEED_TO_VERIFY(HttpStatus.BAD_REQUEST,
-            "AUTH400_4",
-            "이메일 인증을 먼저 완료해주세요."),
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST,
-            "AUTH400_5",
-            "비밀번호가 일치하지 않습니다."),
-    NEED_TO_AGREE(HttpStatus.BAD_REQUEST,
-            "AUTH400_6",
-            "개인정보 보호약관 동의는 필수입니다."),
-    VERIFICATION_EXPIRED(HttpStatus.BAD_REQUEST,
-            "AUTH400_7",
-            "해당 이메일 인증이 만료되었습니다. 이메일 인증 후 30분 내 회원가입을 완료해주세요."),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "AUTH400_8", "linkToken이 유효하지 않습니다."),
-    TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "AUTH400_9","linkToken이 만료되었습니다." ),
-    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "AUTH404_1",
-            "해당 메일로 보낸 인증번호가 존재하지 않습니다. 먼저 원하는 이메일로 인증번호를 보내주세요."),
-    EMAIL_SEND_FAILED(HttpStatus.SERVICE_UNAVAILABLE,
-                "AUTH503_1",
-            "이메일 발송에 실패하였습니다. 메일 서버 장애");
-
+    CODE_NOT_VALIDATE(HttpStatus.BAD_REQUEST, "AUTH400_1", "해당 인증번호는 만료되었습니다. 발급된 시간 기준 5분 이내로 인증번호를 입력해주세요."),
+    CODE_NOT_MATCH(HttpStatus.BAD_REQUEST, "AUTH400_2", "올바르지 않는 인증번호 입니다. 정확한 인증번호 6자리를 입력해주세요."),
+    CODE_ALREADY_USED(HttpStatus.BAD_REQUEST, "AUTH400_3", "이미 인증코드가 사용되었습니다."),
+    NEED_TO_VERIFY(HttpStatus.BAD_REQUEST, "AUTH400_4", "이메일 인증을 먼저 완료해주세요."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "AUTH400_5", "비밀번호가 일치하지 않습니다."),
+    NEED_TO_AGREE(HttpStatus.BAD_REQUEST, "AUTH400_6", "개인정보 보호약관 동의는 필수입니다."),
+    VERIFICATION_EXPIRED(HttpStatus.BAD_REQUEST, "AUTH400_7", "해당 이메일 인증이 만료되었습니다. 이메일 인증 후 30분 내 회원가입을 완료해주세요."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH404_1", "해당 메일로 보낸 인증번호가 존재하지 않습니다. 먼저 원하는 이메일로 인증번호를 보내주세요."),
+    EMAIL_SEND_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "AUTH503_1", "이메일 발송에 실패하였습니다. 메일 서버 장애"),
+    ;
 
     private final HttpStatus status;
     private final String code;
