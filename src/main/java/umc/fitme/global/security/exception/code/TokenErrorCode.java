d11c40485848a876d8c7ecb4cdb71e159a2677ef
kkangmen <pickofee@gmail.com>
2026-08-02 16:06:15 +0900

    feature: AT 블랙리스트 에러코드 추가

--- src/main/java/umc/fitme/global/security/exception/code/TokenErrorCode.java
+++ src/main/java/umc/fitme/global/security/exception/code/TokenErrorCode.java
@@ -18,6 +18,7 @@ public enum TokenErrorCode implements BaseErrorCode {
     AT_EXPIRED(HttpStatus.UNAUTHORIZED,"TOKEN401_1","AT가 만료되었습니다. 재발급을 요청하세요."),
     RT_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN401_2","RT가 만료되었습니다. 재로그인을 해주세요."),
     LT_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN401_3","LT가 만료되었습니다. 계정 연동 요청을 다시 시도해주세요." ),
+    AT_BLACKLISTED(HttpStatus.UNAUTHORIZED,"TOKEN401_4","AT는 로그아웃으로 인해 블랙리스트에 등록되었습니다. 다시 로그인하여 새로운 AT를 발급받아주세요."),
     RT_NOT_FOUND(HttpStatus.NOT_FOUND,"TOKEN404_1" , "쿠키가 비어있습니다. 쿠키에 RT 값이 들어있는지 확인하세요."),
     ;
 
