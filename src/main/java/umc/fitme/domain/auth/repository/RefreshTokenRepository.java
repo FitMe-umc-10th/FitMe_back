13f63d89fca53d6d07c15571ccbca406a1e6ae0f
kkangmen <pickofee@gmail.com>
2026-08-02 16:05:20 +0900

    feature: RT 찾는 쿼리 구현

--- src/main/java/umc/fitme/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/umc/fitme/domain/auth/repository/RefreshTokenRepository.java
@@ -3,7 +3,6 @@ package umc.fitme.domain.auth.repository;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import umc.fitme.domain.auth.entity.RefreshToken;
 import umc.fitme.domain.user.entity.User;
@@ -16,10 +15,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<RefreshToken> findByUser(User user);
 
-    @Query("""
-select rt
-from RefreshToken rt
-where rt.user.id = :userId
-""")
-    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByToken(String token);
 }
