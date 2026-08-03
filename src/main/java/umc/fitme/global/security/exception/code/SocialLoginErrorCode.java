633634f9ed7e16bd71b52f5cb1ca83af4d8c89e1
kkangmen <pickofee@gmail.com>
2026-08-04 00:06:12 +0900

    chore: 에러코드 정리

--- src/main/java/umc/fitme/global/security/exception/code/SocialLoginErrorCode.java
+++ src/main/java/umc/fitme/global/security/exception/code/SocialLoginErrorCode.java
@@ -9,21 +9,24 @@ import umc.fitme.global.apiPayload.code.BaseErrorCode;
 @RequiredArgsConstructor
 public enum SocialLoginErrorCode implements BaseErrorCode {
 
-    PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "SOCIAL_LOGIN404_1",
-            "해당되는 소셜로그인 방법이 존재하지 않습니다."),
-    USER_INFO_NOT_FOUND(HttpStatus.NOT_FOUND,
-            "SOCIAL_LOGIN404_2",
-            "해당되는 유저 정보가 존재하지 않습니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,
             "SOCIAL_LOGIN400_1",
             "기존 이메일이 이미 존재하여 계정 충돌이 일어났습니다."),
     EMAIL_CONFLICT(HttpStatus.BAD_REQUEST,
             "SOCIAL_LOGIN400_2",
             "이미 다른 로그인 수단으로 가입된 이메일입니다. 기존 계정과 연동하시겠습니까?"),
+    UNVERIFIED_EMAIL(HttpStatus.BAD_REQUEST,
+            "SOCIAL_LOGIN400_3",
+            "검증되지 않은 카카오 이메일입니다."),
     DELETED_USER_EMAIL(HttpStatus.UNAUTHORIZED,
             "SOCIAL_LOGIN401_1",
             "탈퇴 처리된 계정입니다. 고객센터에 문의해주세요."),
+    PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "SOCIAL_LOGIN404_1",
+            "해당되는 소셜로그인 방법이 존재하지 않습니다."),
+    USER_INFO_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "SOCIAL_LOGIN404_2",
+            "해당되는 유저 정보가 존재하지 않습니다."),
     ;
 
     private final HttpStatus status;
