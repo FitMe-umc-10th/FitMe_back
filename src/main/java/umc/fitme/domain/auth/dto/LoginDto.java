a1cab1ebb93852505adb36c559109e356e20e649
kkangmen <pickofee@gmail.com>
2026-08-04 14:23:02 +0900

    chore: Swagger 이메일 입력 예시 변경

--- src/main/java/umc/fitme/domain/auth/dto/LoginDto.java
+++ src/main/java/umc/fitme/domain/auth/dto/LoginDto.java
@@ -9,7 +9,7 @@ public class LoginDto {
 
     public record LoginReq(
 
-            @Schema(example = "fitme1234@email.com")
+            @Schema(example = "fitme1234@naver.com")
             @Email(message = "이메일 형식에 맞게 입력해주세요.")
             @NotBlank(message = "이메일은 필수 값입니다.")
             String email,
